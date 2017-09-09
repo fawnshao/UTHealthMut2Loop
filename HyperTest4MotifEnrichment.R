2462f161b408bb27f0299fa34ee04f2efd8cd30f
Fawn Shao <dreambetter@gmail.com>
2017-09-09 17:08:28 -0500

    hello world

--- HyperTest4MotifEnrichment.R
+++ HyperTest4MotifEnrichment.R
@@ -8,6 +8,7 @@ a <- read.table(usrlist, sep = "\t", row.names = 1)
 b <- read.table(backgroud, sep = "\t", row.names = 1)
 pv <- a
 pv <- data.frame(pv, a[,1], a[,1])
+colnames(pv) <- c("hyper.pv", "count", "backgroud.count")
 usrlistsum <- sum(a[,1])
 backgroudsum <- sum(b[,1])
 
@@ -16,4 +17,4 @@ for(i in 1:nrow(a)){
 		backgroudsum - b[rownames(b) == rownames(a)[i],1], usrlistsum, lower.tail = FALSE)
 	pv[i, 2:3] <- c(a[i,1], b[rownames(b) == rownames(a)[i],1])
 }
-write.table(pv, file = paste(outprefix, "phyper.txt", sep = "."), quote = F)
+write.table(pv, file = paste(outprefix, "phyper.txt", sep = "."), quote = F, sep = "\t")
