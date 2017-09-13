415bc034f1eaffed882309f8d5f5141d179907a8
Fawn Shao <dreambetter@gmail.com>
2017-09-13 11:06:58 -0500

    cucu has a fat chip

--- PlotDistance.R
+++ PlotDistance.R
@@ -23,19 +23,19 @@ p0 <- ggplot(a, aes(x = factor(group), y = distance, fill = factor(group))) +
     geom_violin() + ylim(c(-1e4, 1e4)) +
     # xlab("") + ylab("distance") +
     labs(title = "Mutation To TSS", x = "", y = "distance") +
-    annotate("text", label = labels, x = unique(a$group), y = 0)
+    annotate("text", label = unique(a$group), x = unique(a$group), y = 0)
 p1 <- ggplot(b, aes(x = factor(group), y = distance, fill = factor(group))) + 
     geom_violin() + ylim(c(-1e4, 1e4)) +
     labs(title = "Mutation To motif", x = "", y = "distance") +
-    annotate("text", label = labels, x = unique(b$group), y = 0)
+    annotate("text", label = unique(b$group), x = unique(b$group), y = 0)
 p2 <- ggplot(c, aes(x = factor(group), y = distance, fill = factor(group))) + 
     geom_violin() + ylim(c(-1e4, 1e4)) +
     labs(title = "Motif To TSS", x = "", y = "distance") +
-    annotate("text", label = labels, x = unique(c$group), y = 0)
+    annotate("text", label = unique(c$group), x = unique(c$group), y = 0)
 p3 <- ggplot(d, aes(x = factor(group), y = distance, fill = factor(group))) + 
     geom_violin() + ylim(c(-1e4, 1e4)) +
     labs(title = "motif-Mutation To TSS", x = "", y = "distance") +
-    annotate("text", label = labels, x = unique(d$group), y = 0)
+    annotate("text", label = unique(d$group), x = unique(d$group), y = 0)
 png(filename = paste(outpre, "violin.png", sep = "."), width = 2000, height = 2000)
 multiplot(p0, p1, p2, p3, cols = 1)
 dev.off()
---
+++ relatedScripts/PlotProfile.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+if(@ARGV < 1) {
+	die "Usage: perl $0 <input bedtools intersect file>\n";
+}
+
