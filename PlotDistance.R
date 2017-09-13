6c4a8d13669fd9d4f8dc6025d3f54115dad8949d
Fawn Shao <dreambetter@gmail.com>
2017-09-13 16:26:48 -0500

    chip
    fat

--- PlotDistance.R
+++ PlotDistance.R
@@ -20,25 +20,29 @@ colnames(c) <- c("group", "distance")
 colnames(d) <- c("group", "distance")
 
 p0 <- ggplot(a, aes(x = factor(group), y = distance, fill = factor(group))) + 
-    geom_violin() + ylim(c(-1e4, 1e4)) +
+    geom_violin() + ylim(c(-5e3, 5e3)) +
     # xlab("") + ylab("distance") +
     labs(title = "Mutation To TSS", x = "", y = "distance") +
-    annotate("text", label = unique(a$group), x = unique(a$group), y = 0)
+    guides(fill = FALSE)
+    #annotate("text", label = unique(a$group), x = unique(a$group), y = 0)
 p1 <- ggplot(b, aes(x = factor(group), y = distance, fill = factor(group))) + 
-    geom_violin() + ylim(c(-1e4, 1e4)) +
+    geom_violin() + ylim(c(-5e3, 5e3)) +
     labs(title = "Mutation To motif", x = "", y = "distance") +
-    annotate("text", label = unique(b$group), x = unique(b$group), y = 0)
+    guides(fill = FALSE)
+    #annotate("text", label = unique(b$group), x = unique(b$group), y = 0)
 p2 <- ggplot(c, aes(x = factor(group), y = distance, fill = factor(group))) + 
-    geom_violin() + ylim(c(-1e4, 1e4)) +
+    geom_violin() + ylim(c(-5e3, 5e3)) +
     labs(title = "Motif To TSS", x = "", y = "distance") +
-    annotate("text", label = unique(c$group), x = unique(c$group), y = 0)
+    guides(fill = FALSE)
+    #annotate("text", label = unique(c$group), x = unique(c$group), y = 0)
 p3 <- ggplot(d, aes(x = factor(group), y = distance, fill = factor(group))) + 
-    geom_violin() + ylim(c(-1e4, 1e4)) +
+    geom_violin() + ylim(c(-5e3, 5e3)) +
     labs(title = "motif-Mutation To TSS", x = "", y = "distance") +
-    annotate("text", label = unique(d$group), x = unique(d$group), y = 0)
+    guides(fill = FALSE)
+    #annotate("text", label = unique(d$group), x = unique(d$group), y = 0)
 png(filename = paste(outpre, "violin.png", sep = "."), width = 2000, height = 2000)
-multiplot(p0, p1, p2, p3, cols = 1)
+multiplot(p0, p1, p3, p2, cols = 1)
 dev.off()
-pdf(file = paste(outpre, "violin.pdf", sep = "."), width = 30, height = 20)
-multiplot(p0, p1, p2, p3, cols = 1)
+pdf(file = paste(outpre, "violin.pdf", sep = "."), width = 100, height = 40)
+multiplot(p0, p1, p3, p2, cols = 1)
 dev.off()
