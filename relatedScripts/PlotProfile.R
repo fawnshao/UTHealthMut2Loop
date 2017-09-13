5b8a7042f73c9a59591a2f0337102f0f1a0a1653
Fawn Shao <dreambetter@gmail.com>
2017-09-13 15:21:33 -0500

    chip
    fat

--- relatedScripts/PlotProfile.R
+++ relatedScripts/PlotProfile.R
@@ -3,11 +3,10 @@ args <- commandArgs(TRUE)
 input <- args[1]
 library(ggplot2)
 
-a <- read.table(input, sep = "\t")
-coords <- cbind.data.frame(a, a[,3]);
-colnames(coords) <- c("x1", "x2", "y1", "y2");
-p0 <- ggplot(coords, aes(xmin = x1, xmax = x2, ymin = y1, ymax = y2)) + 
-	geom_rect(fill = "red")
+coords <- read.table(input, sep = "\t")
+colnames(coords) <- c("x1", "x2", "y1")
+p0 <- ggplot(coords, aes(xmin = x1, xmax = x2, ymin = 0, ymax = y1, fill = "red")) + 
+	geom_rect(colour = "red")
 png(filename = paste(input, "rect.png", sep = "."), width = 2000, height = 2000)
 print(p0)
 dev.off()
