2fab00dbbafa6fef8d7fa23624dfb07a33da5b91
Fawn Shao <dreambetter@gmail.com>
2017-10-05 15:49:56 -0500

    chip

--- pMUT_EXPR_in_TAD.sh
+++ pMUT_EXPR_in_TAD.sh
@@ -120,12 +120,12 @@ bedtools closest -D b -a $mutationBED -b $tssBED | \
 awk -F "\t" -v var=$promoterLEN -v OFS="\t" \
 '$(NF-6)!="." && $NF > -1 * var && $NF < var {print $1,$2,$3,$4,$10,$13}' > ${outpre}.pMUT
 
-# mutation promoter to TAD
+# mutated promoter to TAD
 # find the overlapped TAD for each mutated promoter.
 bedtools intersect -wao -a ${outpre}.pMUT -b $tadBED | awk '$NF > 0' | cut -f 1-10 > ${outpre}.pMUT.TAD
 
 # promoter to TAD
-# find all the promoters in a TAD.
+# find all the promoters in a TAD, and keeps those with more than 2 genes
 bedtools intersect -wao -a $tssBED -b $tadBED | awk '$NF > 0' | cut -f 1-10 > ${outpre}.p.TAD
 cut -f10 ${outpre}.p.TAD | sort | uniq -c | awk '$1>1{print $2}' > ${outpre}.multiple.p.TAD
 grep -wf ${outpre}.multiple.p.TAD ${outpre}.pMUT.TAD > ${outpre}.multiple.pMUT.TAD
@@ -138,43 +138,53 @@ grep -wf ${outpre}.multiple.p.TAD ${outpre}.pMUT.TAD > ${outpre}.multiple.pMUT.T
 # 	awk -v a=$line '$10==a' ${outpre}.pMUT.TAD >> ${outpre}.multiple.pMUT.TAD
 # done
 
-
+# find the mutated promoter and with neigbors in the same TAD, 
+# and the corresponding sample should have expression
 head -1 $expMAT | awk -F"\t" '{for(i=1;i<=NF;i++){print substr($i,1,15)}}' > ${expMAT}.id
 cut -f 1 ${expMAT}.id | grep -wf - ${outpre}.multiple.pMUT.TAD > ${outpre}.multiple.pMUT.TAD.withexp
 
-TCGAsample="TCGA-AD-A5EJ-01"
-TADid="TAD_3"
-awk -v tad=$TADid '$10==tad{print $4}' ${outpre}.p.TAD | \
-tr ';' '\n' | cut -d "|" -f 1 | sort | uniq > IamGroot.0
-col=`head -1 $expMAT | awk -F"\t" -v sample=$TCGAsample \
-'{for(i=1;i<=NF;i++){if(substr($i,1,15) == sample){print i}}}'`
-
-get_row_column IamGroot.0 1 $expMAT ${outpre}.mutMAT
-# grep -wf IamGroot.0 $expMAT | cut -f 1 > ${outpre}.mutMAT
-cp ${outpre}.mutMAT ${outpre}.ctlMAT
-for i in $col
+# TCGAsample="TCGA-AD-A5EJ-01"
+# TADid="TAD_3"
+cut -f 10 ${outpre}.multiple.pMUT.TAD.withexp | sort | uniq | while read TADid
 do
-	# grep -wf IamGroot.0 $expMAT | cut -f $i > IamGroot.$i
-	get_row_column IamGroot.0 $i $expMAT IamGroot.$i
-	paste ${outpre}.mutMAT IamGroot.$i > IamGroot.IamGroot
-	mv IamGroot.IamGroot ${outpre}.mutMAT
-	rm IamGroot.$i
+	awk -v tad=$TADid '$10==tad{print $4}' ${outpre}.p.TAD | \
+	tr ';' '\n' | cut -d "|" -f 1 | sort | uniq > IamGroot.0
+	
+	awk -v tad=$TADid '$10==tad{print $4}' ${outpre}.multiple.pMUT.TAD.withexp | \
+	cut -d"~" -f1 | sort | uniq | while read TCGAsample
+	do
+		col=`head -1 $expMAT | awk -F"\t" -v sample=$TCGAsample \
+		'{for(i=1;i<=NF;i++){if(substr($i,1,15) == sample){print i}}}'`	
+		get_row_column IamGroot.0 1 $expMAT ${outpre}.mutMAT
+		# grep -wf IamGroot.0 $expMAT | cut -f 1 > ${outpre}.mutMAT
+		cp ${outpre}.mutMAT ${outpre}.ctlMAT
+		for i in $col
+		do
+			# grep -wf IamGroot.0 $expMAT | cut -f $i > IamGroot.$i
+			get_row_column IamGroot.0 $i $expMAT IamGroot.$i
+			paste ${outpre}.mutMAT IamGroot.$i > IamGroot.IamGroot
+			mv IamGroot.IamGroot ${outpre}.mutMAT
+			rm IamGroot.$i
+		done
+
+		awk -v tad=$TADid '$10==tad{print $4}' ${outpre}.pMUT.TAD | \
+		cut -d"~" -f1 | sort | uniq > IamGroot.00
+		head -1 $expMAT | awk -F"\t" '{{for(i=1;i<=NF;i++){o=substr($i,1,15);print i,o}}}' | \
+		grep -vwf IamGroot.00 | awk '{print $1}' | sed -n '2,$p' > IamGroot.000
+		for i in `cat IamGroot.000`
+		do
+			# grep -wf IamGroot.0 $expMAT | cut -f $i > IamGroot.$i
+			get_row_column IamGroot.0 $i $expMAT IamGroot.$i
+			paste ${outpre}.ctlMAT IamGroot.$i > IamGroot.IamGroot
+			mv IamGroot.IamGroot ${outpre}.ctlMAT
+			rm IamGroot.$i
+		done
+
+		Rscript $bindir/Wilcox.test.R ${outpre}.ctlMAT ${outpre}.mutMAT $TADid $TCGAsample $foldchange
+	done
 done
 
-awk -v tad=$TADid '$10==tad{print $4}' ${outpre}.pMUT.TAD | \
-cut -d"~" -f1 | sort | uniq > IamGroot.00
-head -1 $expMAT | awk -F"\t" '{{for(i=1;i<=NF;i++){o=substr($i,1,15);print i,o}}}' | \
-grep -vwf IamGroot.00 | awk '{print $1}' | sed -n '2,$p' > IamGroot.000
-for i in `cat IamGroot.000`
-do
-	# grep -wf IamGroot.0 $expMAT | cut -f $i > IamGroot.$i
-	get_row_column IamGroot.0 $i $expMAT IamGroot.$i
-	paste ${outpre}.ctlMAT IamGroot.$i > IamGroot.IamGroot
-	mv IamGroot.IamGroot ${outpre}.ctlMAT
-	rm IamGroot.$i
-done
 
-Rscript $bindir/Wilcox.test.R ${outpre}.ctlMAT ${outpre}.mutMAT $TADid $TCGAsample $foldchange
 
 
 
