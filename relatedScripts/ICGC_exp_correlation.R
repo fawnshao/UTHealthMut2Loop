bf9d09cce2cea452f40f6462488dbe4e92ff9074
Fawn Shao <dreambetter@gmail.com>
2017-11-20 15:47:40 -0600

    chip

--- relatedScripts/ICGC_exp_correlation.R
+++ relatedScripts/ICGC_exp_correlation.R
@@ -21,9 +21,10 @@ mycor <- function(i, m){
 		gene2 <- rownames(m)[j]
 		cor.x <- cor.test(m[i,],m[j,], method = "pearson")
 		cor.y <- cor.test(m[i,],m[j,], method = "spearman")
-		res[c,] <- c(gene1, gene2, cor.x$estimate[[1]], cor.x$p.value, 
-			cor.y$estimate[[1]], cor.y$p.value)
-		c <- c + 1
+		if(abs(cor.x$estimate[[1]]) > 0.6 || abs(cor.y$estimate[[1]]) > 0.6){
+			res[c,] <- c(gene1, gene2, cor.x$estimate[[1]], cor.x$p.value, cor.y$estimate[[1]], cor.y$p.value)
+			c <- c + 1
+		}
 	}
 	colnames(res) <- c("gene1", "gene2", "pearson.cor", "pearson.pvalues", "spearman.cor", "spearman.pvalues")
 	return(res)
@@ -34,15 +35,15 @@ clusterExport(cl, "mycor")
 # a <- parLapply(cl, 1:nrow(data), function(x) {mycor(x,data)})
 # out.data <- do.call("rbind", a)
 # write.table(out.data, file = out, sep = "\t")
-for(s in 1:floor(nrow(data)/10)){
+for(s in 1:(floor(nrow(data)/10) - 1)){
 	start <- (s - 1) * 10 + 1
 	end <- s * 10
 	a <- parLapply(cl, start:end, function(x) {mycor(x,data)})
 	out.data <- do.call("rbind", a)
-	write.table(out.data, file = out, sep = "\t", append = TRUE)
+	write.table(out.data, file = out, sep = "\t", append = TRUE, quote = FALSE)
 }
-a <- parLapply(cl, (end + 1):nrow(data), function(x) {mycor(x,data)})
+a <- parLapply(cl, (end + 1):(nrow(data)-1), function(x) {mycor(x,data)})
 out.data <- do.call("rbind", a)
-write.table(out.data, file = out, sep = "\t", append = TRUE)
+write.table(out.data, file = out, sep = "\t", append = TRUE, quote = FALSE)
 stopCluster(cl)
 save.image(file = paste(out,"RData", sep = "."))
