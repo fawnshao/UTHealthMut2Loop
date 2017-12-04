bb73eae9271d2fca49e2804bf521e7f2ce02d3f2
Fawn Shao <dreambetter@gmail.com>
2017-12-04 16:42:15 -0600

    chip

--- lm_mut_exp.sh
+++ lm_mut_exp.sh
@@ -8,7 +8,7 @@ module load Rstats
 # tssBED=hg19.refGene.tss.uniq.srt.bed
 # tadBED=hg19.GSE63525_GM12878.srt.bed
 # promoterLEN=1000
-pre=COAD-US
+pre=$1
 matrixdir=/home1/04935/shaojf/stampede2/mutations/ICGC/lm/mut_exp_matrix
 np=68
 
@@ -20,7 +20,7 @@ np=68
 # '$(NF-6)!="." && $NF > -1 * var && $NF < var {print $1":"$2":"$3,$7"~"$10}' | \
 # perl $bindir/relatedScripts/add_any_2files_together.pl $matrixdir/$pre.bothWGS.mut.tsv /dev/stdin 0 0 | cut -f 2,4- >> $pre.p.mut.tsv
 
-count=`wc -l ${outpre}.IamGroot.Rinput | awk '{print $1}'`
+count=`wc -l ${pre}.IamGroot.Rinput | awk '{print $1}'`
 echo +++++++++ Running Rscript to output loop translocate candidates  ++++++++
 # use Z score to find the expression alteration direction in the TAD
 if [ $count -lt 100 ]
@@ -31,8 +31,8 @@ else
 	echo Running in many pieces
 	date
 	each=`echo $count/$np | bc`
-	sed -n '2,$p' ${outpre}.IamGroot.Rinput | split -l $each /dev/stdin ${outpre}.IamGroot.Rinput.
-	for f in ${outpre}.IamGroot.Rinput.*
+	sed -n '2,$p' ${pre}.IamGroot.Rinput | split -l $each /dev/stdin ${pre}.IamGroot.Rinput.
+	for f in ${pre}.IamGroot.Rinput.*
 	do
 		echo "TAD sample mutsample genes mutgene" | cat - $f > $f.title
 		rm $f
@@ -47,5 +47,5 @@ else
 		new=`echo $f | awk -F"." -vOFS="." '{print $1,$5,$6,$7}'`
 		mv $f $new
 	done
-	rm ${outpre}.IamGroot.Rinput.*
+	rm ${pre}.IamGroot.Rinput.*
 fi
