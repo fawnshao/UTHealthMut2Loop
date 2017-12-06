ef7507e2acef40960d2d51691aac585d4725c865
Fawn Shao <dreambetter@gmail.com>
2017-12-05 23:15:39 -0600

    chip

--- relatedScripts/lm_mut_exp_from_matrix.R
+++ relatedScripts/lm_mut_exp_from_matrix.R
@@ -12,12 +12,19 @@ m.gene.name <- rownames(m.data)
 m.sample.name <- as.matrix(read.table(mutmat, sep = "\t", row.names = 1, nrow = 1))
 # m.data[rowSums(m.data)>5,]
 for(i in 1:nrow(m.data)){
-	lm.res <- data.frame()
+	mutation <- c()
+	gene <- c()
+	slop <- c()
+	pvalue <- c()
 	for(j in 1:nrow(data)){
 		lm.fit <- summary(lm(data[j,] ~ m.data[i,]))
-		if(lm.fit$coefficients[2,4] < 0.1){
-			lm.res <- rbind.data.frame(lm.res, c(m.gene.name[i],gene.name[j],lm.fit$coefficients[2,c(1,4)]))
+		if(lm.fit$coefficients[2,4] < 0.1 && !is.na(lm.fit$coefficients[2,4])){
+			mutation <- c(mutation, m.gene.name[i])
+			gene <-c(gene, gene.name[j])
+			slop <- lm.fit$coefficients[2,1]
+			pvalue <- lm.fit$coefficients[2,4]
 		}
 	}
-	write.table(lm.res, file = paste(outpre, "exp2mut.lm", "tsv", sep = "."), sep= "\t", append = T)
+	lm.res <- data.frame(mutation, gene, slop, pvalue)
+	write.table(lm.res, file = paste(outpre, "exp2mut.lm", "tsv", sep = "."), sep= "\t", append = T, row.names = F)
 }
