1bc4fa740f9c821e2893c04917a94f8faf601db2
Fawn Shao <dreambetter@gmail.com>
2017-12-07 13:30:32 -0600

    chip

--- zscore_lm_mut_exp_from_matrix.sh
+++ zscore_lm_mut_exp_from_matrix.sh
@@ -8,6 +8,7 @@ promoterLEN=1000
 winSIZE=500000
 matrixdir=/home1/04935/shaojf/stampede2/mutations/ICGC/lm/mut_exp_matrix
 bindir=/home1/04935/shaojf/myTools/UTHealthMut2Loop
+cordir=/home1/04935/shaojf/stampede2/mutations/ICGC/correlation
 tssBED=/home1/04935/shaojf/stampede2/refs/hg19.refGene.tss.uniq.srt.bed
 
 date
@@ -26,26 +27,24 @@ sed 's/:/\t/g' | sed -n '2,$p' | bedtools sort -i - | \
 bedtools closest -D b -a - -b $tssBED | \
 awk -F "\t" -v var=$promoterLEN -v OFS="\t" \
 '$(NF-6)!="." && $NF > -1 * var && $NF < var {print $1":"$2":"$3,$7"~"$10}' | \
+perl $bindir/relatedScripts/add_any_2files_together.pl $pre.mw.mut.tsv /dev/stdin  0 0 | \
+awk -vOFS="\t" 'NF==4{print $1,$2,$4}' | \
 perl $bindir/relatedScripts/add_any_2files_together.pl $matrixdir/$pre.bothWGS.mut.tsv /dev/stdin  0 0 | \
-cut -f 1-2,4- | sed 's/\t/:/' >> $pre.p.mut.tsv
+cut -f 1-3,5- | sed 's/\t/%/' | sed 's/\t/%/' >> $pre.p.mut.tsv
 
+# Rscript $bindir/relatedScripts/zscore_lm_mut_exp_from_matrix.R \
+# $matrixdir/$pre.bothWGS.exp.tsv $pre.p.mut.tsv $cordir/correlated.$pre.tsv $pre
 
-
-
-
-
-
-
-count=`wc -l $matrixdir/$pre.bothWGS.mut.tsv | awk '{print $1}'`
+count=`wc -l $pre.p.mut.tsv | awk '{print $1}'`
 each=`echo $count/$np | bc`
-sed -n '2,$p' $matrixdir/$pre.bothWGS.mut.tsv | split -l $each /dev/stdin $pre.tmp.
+sed -n '2,$p' $pre.p.mut.tsv | split -l $each /dev/stdin $pre.tmp.
 for f in $pre.tmp.*
 do
-	head -1 $matrixdir/$pre.bothWGS.mut.tsv | cat - $f > $f.title
-	Rscript $bindir/relatedScripts/lm_mut_exp_from_matrix.R $matrixdir/$pre.bothWGS.exp.tsv $f.title $f &
+	head -1 $pre.p.mut.tsv | cat - $f > $f.title
+	Rscript $bindir/relatedScripts/zscore_lm_mut_exp_from_matrix.R $matrixdir/$pre.bothWGS.exp.tsv $f.title $cordir/correlated.$pre.tsv $f &
 done
 wait
-cat $pre.tmp.*.exp2mut.lm.tsv > $pre.exp2mut.lm.tsv
+cat $pre.tmp.*.exp2mut.lm.tsv | sort | uniq > $pre.exp2mut.lm.tsv
 rm $pre.tmp.*
 echo ++++++++finished+++++++
 date
