906b95075d4b4af62a47255cf037b19c60a8ea1d
Fawn Shao <dreambetter@gmail.com>
2018-01-24 18:18:48 -0600

    chip

--- relatedScripts/correlation_for_a_given_pair.R
+++ relatedScripts/correlation_for_a_given_pair.R
@@ -1,20 +1,23 @@
 args <- commandArgs(TRUE)
-pairs <- read.table(args[1], sep = "\t")
+pairs <- as.matrix(read.table(args[1], sep = "\t"))
 exprs <- as.matrix(read.table(args[2], sep = "\t", row.names = 1))
 genes <- rownames(exprs)
-res <- matrix(ncol = 8, nrow = nrow(pairs))
+res <- matrix(ncol = 4, nrow = nrow(pairs))
+ave <- matrix(ncol = 2, nrow = nrow(pairs))
 for(i in 1:nrow(pairs)){
 	x1 <- exprs[genes==pairs[i,1],]
 	x2 <- exprs[genes==pairs[i,2],]
-	cor.x <- cor.test(x1, x2, method = "pearson")
-	cor.y <- cor.test(x1, x2, method = "spearman")
-	res[i,] <- c(pairs[i,], 
-		cor.x$estimate[[1]], cor.x$p.value, 
-		cor.y$estimate[[1]], cor.y$p.value,
-		mean(x1), mean(x2))
+	if(length(x1) == length(x2) && length(x1) == ncol(exprs)){
+		cor.x <- cor.test(x1, x2, method = "pearson")
+		cor.y <- cor.test(x1, x2, method = "spearman")
+		res[i,] <- c(cor.x$estimate[[1]], cor.x$p.value, 
+			cor.y$estimate[[1]], cor.y$p.value)
+	}
+	ave[i,] <- c(mean(x1), mean(x2))
 }
-colnames(res) <- c("gene.rev","gene.fwd",
+out <- data.frame(pairs, ave, res)
+colnames(out) <- c("gene.rev","gene.fwd",
+	"ave.gene.rev","ave.gene.fwd",
 	"pearson.cor", "pearson.pvalues", 
-	"spearman.cor", "spearman.pvalues",
-	"ave.gene.rev","ave.gene.fwd")
-write.table(res, file = paste(args[1], "cor.tsv", sep = "."), sep = "\t", row.names = FALSE, quote = FALSE)
+	"spearman.cor", "spearman.pvalues")
+write.table(out, file = paste(args[1], "cor.tsv", sep = "."), sep = "\t", row.names = FALSE, quote = FALSE)
