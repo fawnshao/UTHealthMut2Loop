46f4cb29bc4fd0fb1860b7bc8c0853e65a461e3b
Fawn Shao <dreambetter@gmail.com>
2018-02-21 13:37:18 -0600

    chip

--- relatedScripts/HKG.TSG.01.matrix.R
+++ relatedScripts/HKG.TSG.01.matrix.R
@@ -3,8 +3,8 @@ args <- commandArgs(TRUE)
 data <- as.matrix(read.table(args[1], sep = "\t", row.names = 1, header = T, na.strings = "/"))
 x <- data
 x[is.na(x)] <- 0
-x[x > 0.1] <- 1
-x[x <= 0.1] <- 0
+x[x >= 1] <- 1
+x[x < 1] <- 0
 # breaklists <- c(seq(0, 2, by = 0.01),seq(2.1, 5.7, by = 0.1))
 # colorn <- length(breaklists)
 # colors <- colorRampPalette(c("blue", "yellow", "red"))(colorn)
