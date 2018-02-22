e5175a9e88d8ba0e446d7a6c588b8811ad32823a
Fawn Shao <dreambetter@gmail.com>
2018-02-22 10:37:11 -0600

    chip

--- referenceCode/eCLIP-Seq.sh
+++ referenceCode/eCLIP-Seq.sh
@@ -40,7 +40,17 @@
 # >Adapter_Trimming
 # CTGTCTCTTATACACATCT
 
+### sub directories
 fastq_dir=/home1/04935/shaojf/scratch/eCLIP_MCF-7/fastqs
+adapterTrimmed=/home1/04935/shaojf/scratch/eCLIP_MCF-7/fastqs_adapterTrim
+fastqc_dir=/home1/04935/shaojf/scratch/eCLIP_MCF-7/fastqc_res
+rmrep=/home1/04935/shaojf/scratch/eCLIP_MCF-7/STAR_rmRep
+mappingtogenome=/home1/04935/shaojf/scratch/eCLIP_MCF-7/STAR_genome
+bw_dir=/home1/04935/shaojf/scratch/eCLIP_MCF-7/bigwigs
+genomesize=/home1/04935/shaojf/scratch/star_index/hg19.chrom.sizes
+
+# for cutadapt, multiple processors
+module load python3
 
 ### for STAR
 hg19file=/home1/04935/shaojf/scratch/star_index/hg19.fa
@@ -54,10 +64,11 @@ STAR --runMode genomeGenerate --runThreadN 68 --genomeDir $hg19indexdir --genome
 STAR --runMode genomeGenerate --runThreadN 68 --genomeDir $repindexdir --genomeFastaFiles $repfile
 
 ## step 1, FastQC
+date
 echo "step 1, FastQC ......"
-mkdir fastqc_res
+mkdir $fastqc_dir
 # fastqc --contaminants $barcodes --adapters $adapters --outdir fastqc_res $fastq_dir/*.fastq
-fastqc --outdir fastqc_res $fastq_dir/*.fastq
+fastqc --outdir $fastqc_dir $fastq_dir/*.fastq
 ### Demultiplexing
 ## don't need to do this
 # for pre in MCF7-CLIP-input MCF7-CLIP-RBM39-IPA MCF7-CLIP-RBM39-IPB
@@ -68,13 +79,14 @@ fastqc --outdir fastqc_res $fastq_dir/*.fastq
 # done
 
 ## step 2, Cutadapt
+date
 echo "step 2, Cutadapt ......"
-adapterTrimmed=fastqs_adapterTrim
 mkdir $adapterTrimmed
 # Cutadapt round 1: Takes output from demultiplexed files. Run to trim off both 5’ and 3’ adapters on both reads
+# with cutadapt v1.15, not use "-f fastq --match-read-wildcards"
 for pre in MCF7-CLIP-input MCF7-CLIP-RBM39-IPA MCF7-CLIP-RBM39-IPB
 do
-	cutadapt -f fastq --match-read-wildcards --times 1 -e 0.1 -O 1 --quality-cutoff 6 -m 18 --nextseq-trim=20 --cores=21 -A a0=NNNNNAGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -g g1=CTTCCGATCTACAAGTT -g g2=CTTCCGATCTTGGTCCT -A a1=AACTTGTAGATCGGA -A a2=AGGACCAAGATCGGA -A a3=ACTTGTAGATCGGAA -A a4=GGACCAAGATCGGAA -A a5=CTTGTAGATCGGAAG -A a6=GACCAAGATCGGAAG -A a7=TTGTAGATCGGAAGA -A a8=ACCAAGATCGGAAGA -A a9=TGTAGATCGGAAGAG -A a10=CCAAGATCGGAAGAG -A a11=GTAGATCGGAAGAGC -A a12=CAAGATCGGAAGAGC -A a13=TAGATCGGAAGAGCG -A a14=AAGATCGGAAGAGCG -A a15=AGATCGGAAGAGCGT -A a16=GATCGGAAGAGCGTC -A a17=ATCGGAAGAGCGTCG -A a18=TCGGAAGAGCGTCGT -A a19=CGGAAGAGCGTCGTG -A a20=GGAAGAGCGTCGTGT -o $adapterTrimmed/${pre}_R1.adapterTrim.fastq -p $adapterTrimmed/${pre}_R2.adapterTrim.fastq $fastq_dir/${pre}_R1.fastq $fastq_dir/${pre}_R2.fastq > $adapterTrimmed/${pre}.fastq.adapterTrim.metrics &
+	/home1/04935/shaojf/.local/bin/cutadapt --times 1 -e 0.1 -O 1 --quality-cutoff 6 -m 18 --nextseq-trim=20 --cores=21 -A a0=NNNNNAGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -g g1=CTTCCGATCTACAAGTT -g g2=CTTCCGATCTTGGTCCT -A a1=AACTTGTAGATCGGA -A a2=AGGACCAAGATCGGA -A a3=ACTTGTAGATCGGAA -A a4=GGACCAAGATCGGAA -A a5=CTTGTAGATCGGAAG -A a6=GACCAAGATCGGAAG -A a7=TTGTAGATCGGAAGA -A a8=ACCAAGATCGGAAGA -A a9=TGTAGATCGGAAGAG -A a10=CCAAGATCGGAAGAG -A a11=GTAGATCGGAAGAGC -A a12=CAAGATCGGAAGAGC -A a13=TAGATCGGAAGAGCG -A a14=AAGATCGGAAGAGCG -A a15=AGATCGGAAGAGCGT -A a16=GATCGGAAGAGCGTC -A a17=ATCGGAAGAGCGTCG -A a18=TCGGAAGAGCGTCGT -A a19=CGGAAGAGCGTCGTG -A a20=GGAAGAGCGTCGTGT -o $adapterTrimmed/${pre}_R1.adapterTrim.fastq -p $adapterTrimmed/${pre}_R2.adapterTrim.fastq $fastq_dir/${pre}_R1.fastq $fastq_dir/${pre}_R2.fastq > $adapterTrimmed/${pre}.fastq.adapterTrim.metrics &
 done
 wait
 # Takes output from cutadapt round 1. Run to trim off the 3’ adapters on read 2, to control for double ligation events
@@ -83,26 +95,68 @@ wait
 # Make also sure that you have pigz (parallel gzip) installed if you use multiple cores and write to a .gz output file. Otherwise, compression of the output will be done in a single thread and therefore be the main bottleneck.
 for pre in MCF7-CLIP-input MCF7-CLIP-RBM39-IPA MCF7-CLIP-RBM39-IPB
 do
-	cutadapt -f fastq --match-read-wildcards --times 1 -e 0.1 -O 5 --quality-cutoff 6 -m 18 --cores=21 -A a1=AACTTGTAGATCGGA -A a2=AGGACCAAGATCGGA -A a3=ACTTGTAGATCGGAA -A a4=GGACCAAGATCGGAA -A a5=CTTGTAGATCGGAAG -A a6=GACCAAGATCGGAAG -A a7=TTGTAGATCGGAAGA -A a8=ACCAAGATCGGAAGA -A a9=TGTAGATCGGAAGAG -A a10=CCAAGATCGGAAGAG -A a11=GTAGATCGGAAGAGC -A a12=CAAGATCGGAAGAGC -A a13=TAGATCGGAAGAGCG -A a14=AAGATCGGAAGAGCG -A a15=AGATCGGAAGAGCGT -A a16=GATCGGAAGAGCGTC -A a17=ATCGGAAGAGCGTCG -A a18=TCGGAAGAGCGTCGT -A a19=CGGAAGAGCGTCGTG -A a20=GGAAGAGCGTCGTGT -o $adapterTrimmed/${pre}_R1.adapterTrim.round2.fastq -p $adapterTrimmed/${pre}_R2.adapterTrim.round2.fastq $adapterTrimmed/${pre}_R1.adapterTrim.fastq $adapterTrimmed/${pre}_R2.adapterTrim.fastq > $adapterTrimmed/${pre}.fastq.adapterTrim.round2.metrics &
+	/home1/04935/shaojf/.local/bin/cutadapt --times 1 -e 0.1 -O 5 --quality-cutoff 6 -m 18 --cores=21 -A a1=AACTTGTAGATCGGA -A a2=AGGACCAAGATCGGA -A a3=ACTTGTAGATCGGAA -A a4=GGACCAAGATCGGAA -A a5=CTTGTAGATCGGAAG -A a6=GACCAAGATCGGAAG -A a7=TTGTAGATCGGAAGA -A a8=ACCAAGATCGGAAGA -A a9=TGTAGATCGGAAGAG -A a10=CCAAGATCGGAAGAG -A a11=GTAGATCGGAAGAGC -A a12=CAAGATCGGAAGAGC -A a13=TAGATCGGAAGAGCG -A a14=AAGATCGGAAGAGCG -A a15=AGATCGGAAGAGCGT -A a16=GATCGGAAGAGCGTC -A a17=ATCGGAAGAGCGTCG -A a18=TCGGAAGAGCGTCGT -A a19=CGGAAGAGCGTCGTG -A a20=GGAAGAGCGTCGTGT -o $adapterTrimmed/${pre}_R1.adapterTrim.round2.fastq -p $adapterTrimmed/${pre}_R2.adapterTrim.round2.fastq $adapterTrimmed/${pre}_R1.adapterTrim.fastq $adapterTrimmed/${pre}_R2.adapterTrim.fastq > $adapterTrimmed/${pre}.fastq.adapterTrim.round2.metrics &
 done
 wait
 
 ## step 3, STAR rmRep
+date
 echo "step 3, STAR rmRep ......"
-adapterTrimmed=fastqs_adapterTrim
-mkdir $adapterTrimmed
+mkdir $rmrep
 # STAR rmRep: Takes output from cutadapt round 2. Maps to human specific version of RepBase used to remove repetitive elements, helps control for spurious artifacts from rRNA (& other) repetitive reads.
+# remove --outSAMunmapped Within --outStd BAM_Unsorted > $rmrep/${pre}.round2.rep.bam
+for pre in MCF7-CLIP-input MCF7-CLIP-RBM39-IPA MCF7-CLIP-RBM39-IPB
+do
+	STAR --runMode alignReads --runThreadN 68 \
+	--genomeDir $repindexdir --genomeLoad LoadAndRemove \
+	--readFilesIn $adapterTrimmed/${pre}_R1.adapterTrim.round2.fastq \
+	$adapterTrimmed/${pre}_R2.adapterTrim.round2.fastq \
+	--outFilterMultimapNmax 30 \
+	--outFilterMultimapScoreRange 1 \
+	--outFileNamePrefix $rmrep/${pre}.round2.rep. \
+	--outSAMattributes All --outSAMtype BAM Unsorted \
+	--outFilterType BySJout --outReadsUnmapped Fastx \
+	--outFilterScoreMin 10 --outSAMattrRGline ID:foo \
+	--alignEndsType EndToEnd
 
+	samtools view $rmrep/${pre}.round2.rep.Aligned.out.bam | count_aligned_from_sam.py > $rmrep/${pre}.round2.rep.metrics
+	fastqc --outdir $fastqc_dir $rmrep/${pre}.round2.rep.Unmapped.out.mate1 > $rmrep/${pre}.round2.rep.Unmapped.out.mate1.dummy_fastqc &
+	fastqc --outdir $fastqc_dir $rmrep/${pre}.round2.rep.Unmapped.out.mate2 > $rmrep/${pre}.round2.rep.Unmapped.out.mate2.dummy_fastqc &
+	fastq-sort --id $rmrep/${pre}.round2.rep.Unmapped.out.mate1 > $rmrep/${pre}.round2.rep.Unmapped.out.sorted.mate1 &
+	fastq-sort --id $rmrep/${pre}.round2.rep.Unmapped.out.mate2 > $rmrep/${pre}.round2.rep.Unmapped.out.sorted.mate2 &
+done
+wait
 
-STAR --runMode alignReads --runThreadN 16 --genomeDir
-/path/to/RepBase_human_database_file --genomeLoad LoadAndRemove --
-readFilesIn
-/full/path/to/files/file_R1.C01.fastq.gz.adapterTrim.round2.fastq.gz
-/full/path/to/files/file_R2.C01.fastq.gz.adapterTrim.round2.fastq.gz --
-outSAMunmapped Within --outFilterMultimapNmax 30 --outFilterMultimapScoreRange 1 --outFileNamePrefix
-/full/path/to/files/file_R1.C01.fastq.gz.adapterTrim.round2.rep.bam --
-outSAMattributes All --readFilesCommand zcat --outStd BAM_Unsorted --
-outSAMtype BAM Unsorted --outFilterType BySJout --outReadsUnmapped
-Fastx --outFilterScoreMin 10 --outSAMattrRGline ID:foo --alignEndsType
-EndToEnd >
-/full/path/to/files/file_R1.C01.fastq.gz.adapterTrim.round2.rep.bam
+## step 4, STAR genome
+date
+echo "step 4, STAR genome ......"
+mkdir $mappingtogenome
+for pre in MCF7-CLIP-input MCF7-CLIP-RBM39-IPA MCF7-CLIP-RBM39-IPB
+do
+	STAR --runMode alignReads --runThreadN 68 \
+	--genomeDir $hg19indexdir --genomeLoad LoadAndRemove \
+	--readFilesIn $rmrep/${pre}.round2.rep.Unmapped.out.sorted.mate1 \
+	$rmrep/${pre}.round2.rep.Unmapped.out.sorted.mate2 \
+	--outSAMunmapped Within \
+	--outFilterMultimapNmax 1 \
+	--outFilterMultimapScoreRange 1 \
+	--outFileNamePrefix $mappingtogenome/${pre}.round2.rmRep.genome. \
+	--outSAMattributes All --outSAMtype BAM Unsorted \
+	--outFilterType BySJout --outReadsUnmapped Fastx \
+	--outFilterScoreMin 10 --outSAMattrRGline ID:foo \
+	--alignEndsType EndToEnd
+done
+
+## step 5, process STAR results
+# Barcode_collapse_pe: takes output from STAR genome mapping. Custom random-mer-aware script for PCR duplicate removal
+date
+echo "step 5, process STAR results ......"
+mkdir $bw_dir
+for pre in MCF7-CLIP-input MCF7-CLIP-RBM39-IPA MCF7-CLIP-RBM39-IPB
+do
+	barcode_collapse_pe.py --bam $mappingtogenome/${pre}.round2.rmRep.genome.Aligned.out.bam --out_file $mappingtogenome/${pre}.round2.rmRep.genome.rmDup.bam --metrics_file $mappingtogenome/${pre}.round2.rmRep.genome.rmDup.metrics
+	picard SortSam INPUT=$mappingtogenome/${pre}.round2.rmRep.genome.bam OUTPUT=$mappingtogenome/${pre}.round2.rmRep.genome.sorted.bam VALIDATION_STRINGENCY=SILENT SO=coordinate CREATE_INDEX=true
+	samtools index -@ 68 $mappingtogenome/${pre}.round2.rmRep.genome.sorted.bam
+	samtools view -hb -f 128 -@ 68 $mappingtogenome/${pre}.round2.rmRep.genome.sorted.bam > $mappingtogenome/${pre}.round2.rmRep.genome.sorted.r2.bam
+	make_bigwig_files.py --bam $mappingtogenome/${pre}.round2.rmRep.genome.sorted.r2.bam --genome $genomesize --bw_pos $bw_dir/${pre}.round2.rmRep.genome.sorted.r2.norm.pos.bw --bw_neg $bw_dir/${pre}.round2.rmRep.genome.sorted.r2.norm.neg.bw
+done
---
+++ relatedScripts/HKG.TSG.v1.6.R
@@ -0,0 +1,266 @@
+library(data.table)
+library(pheatmap)
+
+args <- c("GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct", 
+	"GTEx_sample.tissue.txt", "v1.6", "0.25", "0.5", "0.1", "10")
+# expression tissuename outputpre tissueTau sampleTau expressioncutoff[raw(tpm+1)]
+# use the raw TPM value
+
+### some functions ###
+###+++###
+fmedian <- function(x)
+	{
+		if(!all(is.na(x)))
+	 	{
+	 		res <- median(x, na.rm = TRUE)
+	 	} else {
+	 		res <- NA
+	 	}
+	 	return(res)
+	}
+###***###***###	
+
+###+++###	
+#Function require a vector with expression of one gene in different tissues/samples.
+#Mean is calculated taking in account tissues with 0 expression. 2+0+4=2
+fmean <- function(x)
+	{
+		if(!all(is.na(x)))
+	 	{
+	 		res <- mean(x, na.rm = TRUE)
+	 	} else {
+	 		res <- NA
+	 	}
+	 	return(res)
+	}
+###***###***###	
+
+###+++###	
+#Function require a vector with expression of one gene in different tissues/samples.
+#Max is calculated taking in account tissues with 0 expression. 2+0+4=2
+fmax <- function(x)
+	{
+		if(!all(is.na(x)))
+	 	{
+	 		res <- max(x, na.rm = TRUE)
+	 	} else {
+	 		res <- NA
+	 	}
+	 	return(res)
+	}
+###***###***###	
+
+###+++###
+#Function require a vector with expression of one gene in different tissues/samples.
+#If expression for one tissue is not known, gene specificity for this gene is NA
+#Minimum 2 tissues/samples
+fTau <- function(x)
+{
+	if(all(!is.na(x)))
+ 	{
+ 		if(min(x, na.rm = TRUE) >= 0)
+		{
+ 			mx <- fmax(x)
+ 			if(mx != 0 && !is.na(mx))
+ 			{
+ 				x <- (1-(x/mx))
+ 				res <- sum(x, na.rm = TRUE)
+ 				res <- res/(length(x)-1)
+ 			} else {
+ 				res <- 0
+ 			}
+ 		} else {
+ 		res <- NA
+ 		#print("Expression values have to be positive!")
+ 		} 
+ 	} else {
+ 		res <- NA
+ 		#print("No data for this gene avalable.")
+ 	} 
+ 	return(res)
+}
+###***###***###
+
+## read in data
+# tpm <- as.matrix(read.table(args[1], sep = "\t", row.names = 1, header = T))
+print("Reading Data")
+raw.table <- fread(args[1], sep = "\t", header = T)
+# Read 56202 rows and 11689 (of 11689) columns from 2.622 GB file in 00:06:16
+tpm <- as.matrix(raw.table[,-1])
+rownames(tpm) <- as.matrix(raw.table[,1])
+info <- as.matrix(read.table(args[2], sep = "\t"))
+tissues <- unique(info[,2])
+tissues <- tissues[-c(7,24,25,53)]
+
+outputpre <- args[3]
+tissueTau <- as.numeric(args[4])
+sampleTau <- as.numeric(args[5])
+nullexpression <- as.numeric(args[6])
+############
+medianthreshold <- as.numeric(args[7])
+############
+
+## calculate
+print("Calculating nullcount, median/mean and Tau for each tissue")
+nullcount <- matrix(ncol = length(tissues), nrow = nrow(tpm))
+rawtpm.median <- matrix(ncol = length(tissues), nrow = nrow(tpm))
+rawtpm.mean <- matrix(ncol = length(tissues), nrow = nrow(tpm))
+rawtpm.Tau <- matrix(ncol = length(tissues), nrow = nrow(tpm))
+for(i in 1:length(tissues)){
+	z <- tpm[, info[,2] == tissues[i]]
+	nullcount[,i] <- apply(z, 1, function(x) {length(x[x < nullexpression])})
+	rawtpm.median[,i] <- apply(z, 1, fmedian)
+	rawtpm.mean[,i] <- apply(z, 1, fmean)
+	rawtpm.Tau[,i] <- apply(z, 1, fTau)
+}
+colnames(nullcount) <- tissues
+colnames(rawtpm.median) <- tissues
+colnames(rawtpm.mean) <- tissues
+colnames(rawtpm.Tau) <- tissues
+
+print("Calculating nullcount, mean and Tau across tissues")
+rawtpm.median.mean <- apply(rawtpm.median, 1, fmean)
+rawtpm.median.Tau <- apply(rawtpm.median, 1, fTau)
+rawtpm.mean.mean <- apply(rawtpm.mean, 1, fmean)
+rawtpm.mean.Tau <- apply(rawtpm.mean, 1, fTau)
+
+nullcount.sum <- apply(nullcount, 1, sum)
+rawtpm.Tau.max <- apply(rawtpm.Tau, 1, function(x) {max(x[is.finite(x)], na.rm = T)})
+rawtpm.Tau.min <- apply(rawtpm.Tau, 1, function(x) {min(x[is.finite(x)], na.rm = T)})
+
+### it takes ~30min until this step
+
+# print("Saving RData")
+# save.image("v1.3.Tau.RData")
+# ### it takes ~50min until this step
+
+print("Writing raw output files")
+all.stats <- data.frame(rownames(tpm), rawtpm.mean.mean, rawtpm.mean.Tau, 
+	rawtpm.median.mean, rawtpm.median.Tau, 
+	nullcount.sum, rawtpm.Tau.max, rawtpm.Tau.min, 
+	rawtpm.mean, rawtpm.median, rawtpm.Tau)
+rownames(all.stats) <- rownames(tpm)
+write.table(data.frame(rownames(tpm), nullcount), file = paste(outputpre, "nullcount.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(data.frame(rownames(tpm), rawtpm.mean), file = paste(outputpre, "rawtpm.mean.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(data.frame(rownames(tpm), rawtpm.median), file = paste(outputpre, "rawtpm.median.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(data.frame(rownames(tpm), rawtpm.Tau), file = paste(outputpre, "rawtpm.Tau.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(all.stats, file = paste(outputpre, "allstats.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+
+#load("v1.3.Tau.RData")
+#library(pheatmap)
+print("Looking for housekeeping genes")
+housekeepinggene <- all.stats[nullcount.sum == 0 & !is.na(rawtpm.Tau.max) & rawtpm.Tau.max < sampleTau & !is.na(rawtpm.median.Tau) & rawtpm.median.Tau < tissueTau & rawtpm.median.min > medianthreshold, ]
+housekeepinggene.median <- rawtpm.median[nullcount.sum == 0 & !is.na(rawtpm.Tau.max) & rawtpm.Tau.max < sampleTau & !is.na(rawtpm.median.Tau) & rawtpm.median.Tau < tissueTau & rawtpm.median.min > medianthreshold, ]
+housekeepinggene.Tau <- rawtpm.Tau[nullcount.sum == 0 & !is.na(rawtpm.Tau.max) & rawtpm.Tau.max < sampleTau & !is.na(rawtpm.median.Tau) & rawtpm.median.Tau < tissueTau & rawtpm.median.min > medianthreshold, ]
+rownames(housekeepinggene.median) <- housekeepinggene[,1]
+rownames(housekeepinggene.Tau) <- housekeepinggene[,1]
+# housekeepinggene[grep("GAPDH", as.matrix(housekeepinggene[,1])), 1]
+
+## check ENSG00000204983.8|PRSS1 Pancreas
+# tissuespecificgene[grep("PRSS1",as.matrix(tissuespecificgene[,1])),1]
+# raw.table[grep("PRSS1",as.matrix(raw.table[,1])),1:5][10,]
+# ENSG00000153266.8|FEZF2	Brain
+# ENSG00000055957.6|ITIH1	Liver
+# ENSG00000136574.13|GATA4	Artery - Coronary,Heart - Atrial Appendage,Heart - Left Ventricle,Ovary,Testis
+print("Looking for tissue specific genes")
+tissuespecificgene <- data.frame()
+tissueflags <- c()
+rindex <- c()
+for(i in 1:nrow(all.stats)){
+	if(!is.na(rawtpm.median.Tau[i]) && rawtpm.median.Tau[i] > 1 - tissueTau && !is.na(rawtpm.Tau.min[i]) && rawtpm.Tau.min[i] < sampleTau && min(nullcount[i,], na.rm = T) == 0){
+		flag <- 0
+		tflags <- c()
+		for(j in 1:length(tissues)){
+			temp <- rawtpm.median[i,j]/fmax(rawtpm.median[i,])
+			if(nullcount[i,j] == 0 && !is.na(rawtpm.Tau[i,j]) && rawtpm.Tau[i,j] < sampleTau &&  !is.na(temp) && temp > 1 - tissueTau && rawtpm.median[i,j] > medianthreshold){
+				flag <- 1
+				tflags <- c(tflags, tissues[j])
+			}
+		}
+		if(flag == 1){
+			tissuespecificgene <- rbind.data.frame(tissuespecificgene, all.stats[i,])
+			tissueflags <- c(tissueflags, paste(tflags, collapse = ","))
+			rindex <- c(rindex, i)
+		}
+	}
+}
+tissuespecificgene.median <- rawtpm.median[rindex, ]
+tissuespecificgene.Tau <- rawtpm.Tau[rindex, ]
+rownames(tissuespecificgene.median) <- tissuespecificgene[,1]
+rownames(tissuespecificgene.Tau) <- tissuespecificgene[,1]
+
+print("Writing HKG/TSG output files")
+write.table(housekeepinggene, file = paste(outputpre, "HKG.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(data.frame(tissuespecificgene, tissueflags), file = paste(outputpre, "TSG.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+
+print("Clustering")
+breaklists <- c(seq(0, 1, by = 0.01))
+colorn <- length(breaklists)
+colors <- colorRampPalette(c("blue", "yellow", "red"))(colorn)
+
+breaklists2 <- c(seq(0, 6, by = 0.05),seq(6.1, 16, by = 0.1))
+colorn2 <- length(breaklists2)
+colors2 <- colorRampPalette(c("blue", "yellow", "red"))(colorn2)
+
+png(filename = paste(outputpre, "HKG.median.png", sep = "."), width = 1000, height = 1500)
+data <- data.frame(housekeepinggene.median, housekeepinggene[,4])
+p1 <- pheatmap(data, scale = "none", show_rownames = F, show_colnames = T, 
+         color = colors2, cluster_cols = T, breaks = breaklists2,
+         clustering_distance_cols = "euclidean", clustering_distance_rows = "euclidean", 
+         clustering_method = "ward.D2"
+         )
+dev.off()
+cluster <- cutree(p1$tree_row, k = 5)
+write.table(data.frame(cluster[p1$tree_row$order], 
+	rownames(data)[p1$tree_row$order],
+	data[p1$tree_row$order, p1$tree_col$order]), 
+	file = paste(outputpre, "HKG.median.pheatmap.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+
+png(filename = paste(outputpre, "HKG.Tau.png", sep = "."), width = 1000, height = 1500)
+data <- data.frame(housekeepinggene.Tau,housekeepinggene[,5])
+pheatmap(data[p1$tree_row$order,p1$tree_col$order], scale = "none", 
+	show_rownames = F, show_colnames = T, color = colors, 
+	cluster_cols = F, cluster_rows = F, breaks = breaklists)
+dev.off()
+write.table(data.frame(cluster[p1$tree_row$order], 
+	rownames(data)[p1$tree_row$order],
+	data[p1$tree_row$order, p1$tree_col$order]), 
+	file = paste(outputpre, "HKG.Tau.pheatmap.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+
+png(filename = paste(outputpre, "TSG.median.png", sep = "."), width = 1000, height = 1500)
+data <- data.frame(tissuespecificgene.median, tissuespecificgene[,4])
+p2 <- pheatmap(data, scale = "none", show_rownames = F, show_colnames = T, 
+         color = colors2, cluster_cols = T, breaks = breaklists2,
+         clustering_distance_cols = "euclidean", clustering_distance_rows = "euclidean", 
+         clustering_method = "ward.D2"
+         )
+dev.off()
+cluster <- cutree(p2$tree_row, k = 5)
+write.table(data.frame(cluster[p2$tree_row$order], 
+	tissueflags[p2$tree_row$order],
+	rownames(data)[p2$tree_row$order],
+	data[p2$tree_row$order, p2$tree_col$order]), 
+	file = paste(outputpre, "TSG.median.pheatmap.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+
+png(filename = paste(outputpre, "TSG.Tau.png", sep = "."), width = 1000, height = 1500)
+data <- data.frame(tissuespecificgene.Tau, tissuespecificgene[,5])
+pheatmap(data[p2$tree_row$order,p2$tree_col$order], scale = "none", 
+	show_rownames = F, show_colnames = T, color = colors, 
+	cluster_cols = F, cluster_rows = F, breaks = breaklists)
+dev.off()
+write.table(data.frame(cluster[p2$tree_row$order], 
+	rownames(data)[p2$tree_row$order],
+	data[p2$tree_row$order, p2$tree_col$order]), 
+	file = paste(outputpre, "TSG.Tau.pheatmap.tsv", sep = "."), 
+	sep = "\t", row.names = FALSE, quote = FALSE)
+
