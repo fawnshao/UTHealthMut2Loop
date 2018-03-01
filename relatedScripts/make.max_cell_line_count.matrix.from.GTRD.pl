44d53a7a5f38998591fdeb5766136f20d77be0e6
Fawn Shao <dreambetter@gmail.com>
2018-03-01 11:25:05 -0600

    chip

--- relatedScripts/make.max_cell_line_count.matrix.from.GTRD.pl
+++ relatedScripts/make.max_cell_line_count.matrix.from.GTRD.pl
@@ -10,6 +10,31 @@ while(<IN>){
 	$tfs{$t[3]} = 1;
 	$genes{$t[9]} = 1;
 	$id = $t[3].":".$t[9];
-	if(not exists)
+	if(not exists $maxcount{$id}){
+		$maxcount{$id} = $t[4];
+	}
+	elsif($t[4] > $maxcount{$id}){
+		$maxcount{$id} = $t[4];
+	}
 }
 close(IN);
+
+print "Gene";
+foreach $tf(sort keys %tfs){
+	print "\t$tf";
+}
+print "\n";
+
+foreach $gene(sort keys %genes){
+	print "$gene";
+	foreach $tf(sort keys %tfs){
+		$id = $tf.":".$gene;
+		if(not exists $maxcount{$id}){
+			print "\t0";
+		}
+		else{
+			print "\t$maxcount{$id}";
+		}
+	}
+	print "\n";
+}
