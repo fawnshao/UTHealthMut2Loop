93d97ac9c517c8618719be2b4b9eac51c72942cc
Fawn Shao <dreambetter@gmail.com>
2018-03-01 12:59:12 -0600

    chip

--- relatedScripts/make.matrix.from.homer.motif.file.pl
+++ relatedScripts/make.matrix.from.homer.motif.file.pl
@@ -6,7 +6,7 @@ $line = <IN>;
 chomp $line;
 @t = split(/\t/,$line);
 print "$t[0]";
-for($i = 1; $i < @t; $i++){
+for($i = 1; $i < scalar(@t); $i++){
 	print "\t$t[$i]";
 }
 print "\n";
@@ -14,14 +14,14 @@ while(<IN>){
 	chomp;
 	@t = split(/\t/);
 	print "$t[0]";
-	print STDERR "\@t\n";
-	for($i = 1; $i < @t; $i++){
+	# print STDERR "@t\n";
+	for($i = 1; $i < scalar(@t); $i++){
 		if($t[$i] eq ''){
 			$a = 0;
 		}
 		else{
 			@tt = split(/\),/, $t[$i]);
-			$a = @tt;
+			$a = scalar(@tt);
 		}
 		print "\t$a";
 	}
