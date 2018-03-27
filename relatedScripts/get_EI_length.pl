58da80a09ce44f58aee4b80bacb09bf64c949058
Fawn Shao <dreambetter@gmail.com>
2018-03-27 09:35:07 -0500

    chip

---
+++ relatedScripts/get_EI_length.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+%gene = ();
+%intron = ();
+open(IN, $ARGV[0]);
+while(<IN>){
+	chomp;
+	@t = split(/\t/);
+	$gene{$t[3]} = $t[2] - $t[1] + 1;
+}
+close(IN);
+
+open(IN, $ARGV[1]);
+while(<IN>){
+	chomp;
+	@t = split(/\t/);
+	if(not exists $intron{$t[3]}){
+		$intron{$t[3]} = 0;
+	}
+	$intron{$t[3]} += $t[2] - $t[1] + 1;
+}
+close(IN);
+
+foreach $g(keys %gene){
+	if(not exists $intron{$g}){
+		$intron{$g} = 0;
+	}
+	$x = $gene{$g} - $intron{$g};
+	print "$g\t$x\n";
+}
