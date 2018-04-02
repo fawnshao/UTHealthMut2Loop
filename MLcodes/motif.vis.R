932a942b4f07bac633b1793f1fca928a7336c3b0
Fawn Shao <dreambetter@gmail.com>
2018-04-02 15:52:23 -0500

    chip

--- MLcodes/motif.vis.R
+++ MLcodes/motif.vis.R
@@ -39,19 +39,27 @@ dev.off()
 
 
 ################################
+library(ggplot2)
+library(pheatmap)
+library(data.table)
+
 args <- c("hkg.vert.known.ETS.Sp1.YY1.bin.txt", 
 	"mixTSG.vert.known.ETS.Sp1.YY1.bin.txt", 
 	"singleTSG.vert.known.ETS.Sp1.YY1.bin.txt")
 input1 <- fread(args[1], sep = "\t", header = F, na.strings = "/")
 input2 <- fread(args[2], sep = "\t", header = F, na.strings = "/")
 input3 <- fread(args[3], sep = "\t", header = F, na.strings = "/")
+percentage <- c(data.matrix(input1[,3])[,1]/2551, 
+	data.matrix(input2[,3])[,1]/1574, 
+	data.matrix(input3[,3])[,1]/2735)
+class <- c(rep("hkg",nrow(input1)), rep("mixTSG",nrow(input2)), rep("singleTSG",nrow(input3)))
+datax <- data.frame(rbind.data.frame(input1[,1:2], input2[,1:2], input3[,1:2]), class, percentage)
+colnames(datax)[1:2] <- c("motif", "bin")
 
-motifs <- as.matrix(input[,1])
-motifs.seq <- as.matrix(input[,2])
-# qv <- data.matrix(input[,c(3,5,7,9,11,13,15)])
-# rv <- data.matrix(input[,c(4,6,8,10,12,14,16)])
-qv <- data.matrix(input[,c(3,5,7)])
-rv <- data.matrix(input[,c(4,6,8)])
-class <- gsub(".q", "", colnames(qv))
-colnames(qv) <- class
-
+myplot <- ggplot(data = datax, aes(x = bin, y = percentage)) + 
+		geom_bar(stat = "identity", fill = "steelblue") +
+		facet_grid(motif ~ .) +
+		ggtitle(args[1])
+png(filename = paste(args[1], "motif.bar.png", sep = "."), width = 700, height = 1500)
+print(myplot)
+dev.off()
