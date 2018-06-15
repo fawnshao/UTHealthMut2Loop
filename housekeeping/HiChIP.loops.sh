0bc6b9a28e91aac436656ea692a1708a8c9ce5a4
Fawn Shao <dreambetter@gmail.com>
2018-06-15 16:16:12 -0500

    chip

--- housekeeping/HiChIP.loops.sh
+++ housekeeping/HiChIP.loops.sh
@@ -7,14 +7,21 @@ bedperl=/home1/04935/shaojf/myTools/UTHealthMut2Loop/housekeeping/get.HiChIP.loo
 for f in *_H3K27ac_Loops.txt
 do
 	pre=`echo $f | cut -f 1 -d"_"`
-	tail -n +2 $f | awk -v var=$pre '{print $1":"$2"-"$3"%"$1":"$2"-"$3"\t"var"\t1"}' >> tmp.txt
+	tail -n +2 $f | awk -v var=$pre '{print $1":"$2"-"$3"%"$4":"$5"-"$6"\t"var"\t1"}' >> tmp.txt
 done
 perl $matperl tmp.txt > HiChIP.loops.mat
-
+rm tmp.txt
 # tail -n +2 HiChIP.loops.mat | awk -vOFS="\t" '{print $1,$1}' | sed 's/%/\t/' | awk '{print $1"\t"$3"\n"$2"\t"$3}' | uniq | sed 's/:/\t/;s/-/\t/' | awk '{print "chr"$0}' | bedtools sort -i - > HiChIP.loops.bed
 tail -n +2 HiChIP.loops.mat | awk '{print $1}' | sed 's/%/\t/' | awk '{print $1"\t"$1"\n"$2"\t"$2}' | uniq | sed 's/:/\t/;s/-/\t/' | awk '{print "chr"$0}' | bedtools sort -i - > HiChIP.loops.bed
 # bedtools intersect -wao -a <(awk -vOFS="\t" '{print $1,$2-5000,$3+5000,$4}' HiChIP.loops.bed) -b <(awk -F"\t" -vOFS="\t" '{a=$2-5000;b=$2+5000;if($6=="-"){a=$3-5000;b=$3+5000}if(a<0){a=0;}print $1,a,b,$4,$5,$6}' gencode.v19.transcript.bed | bedtools sort -i -) > HiChIP.loops.transcript
 bedtools intersect -wao -a HiChIP.loops.bed -b <(awk -F"\t" -vOFS="\t" '{a=$2-5000;b=$2+5000;if($6=="-"){a=$3-5000;b=$3+5000}if(a<0){a=0;}print $1,a,b,$4,$5,$6}' gencode.v19.transcript.bed | bedtools sort -i -) > HiChIP.loops.transcript
 perl $bedperl HiChIP.loops.transcript > HiChIP.loops.transcript.sim
 
+head -1 HiChIP.loops.mat | awk '{print $0"\tGeneAnnotation"}' > HiChIP.loops.mat.annotation
+perl $myperl HiChIP.loops.transcript.sim <(sed 's/%/\t/' HiChIP.loops.mat | tail -n +2) 0 0 | cut -f 1-7,9 | perl $myperl HiChIP.loops.transcript.sim /dev/stdin 0 1 | cut -f 1-8,10 | sed 's/\t/%/' >> HiChIP.loops.mat.annotation
+# head -1 HiChIP.loops.mat.annotation > HiChIP.loops.mat.annotation.diffloop
+# sed 's/%/\t/' HiChIP.loops.mat.annotation | tail -n +2 | awk -F"\t" '$1!=$2' >> HiChIP.loops.mat.annotation.diffloop
+
+echo "ID cell.count GeneAnnotation1 GeneAnnotation2" | tr " " "\t" > HiChIP.loops.mat.annotation.sim
+tail -n +2 HiChIP.loops.mat.annotation | awk -vOFS="\t" '{sum=0;for(i=2;i<=6;i++){sum+=$i}print $1,sum,$7,$8}' >> HiChIP.loops.mat.annotation.sim
 
---
+++ housekeeping/add.annotation.to.a.combination.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+%hash = ();
+open(IN, $ARGV[0]);
+while (<IN>) {
+	chomp;
+	@t = split(/\t/);
+	$hash{$t[0]} = $t[1];
+}
+close(IN);
+
+open(IN, $ARGV[1]);
+while (<IN>) {
+	chomp;
+	@t = split(/\t/);
+	$a = "";
+	$b = "";
+	if($t[2] ne "na"){
+		@tt = split(/,/, $t[2]);
+		foreach $x(@tt){
+			$a = $hash{$x}.",";
+		}
+		$a =~ s/,$//;
+	}
+	if($t[3] ne "na"){
+		@tt = split(/,/, $t[3]);
+		foreach $x(@tt){
+			$b = $hash{$x}.",";
+		}
+		$b =~ s/,$//;
+	}
+	print "$_\t$a\t$b\n";
+}
+close(IN);
