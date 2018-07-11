08674b84d7ee47711049dbaf2e3fac3f1670fe5c
Fawn Shao <dreambetter@gmail.com>
2018-07-11 12:10:52 -0500

    chip

--- housekeeping/HKG.TSG.v2.features.sh
+++ housekeeping/HKG.TSG.v2.features.sh
@@ -333,8 +333,178 @@ do
 done
 wait
 
+################################################ ENCODE ChIP
+# awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19"' metadata.tsv | cut -f 1,7,13,25 > hg19.bigwig.tsv
+# awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19" && $13=="EP300-human"' ../metadata.tsv | cut -f 37 | xargs -n 1 curl -O -L
+# awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19" && $13=="YY1-human"' ../metadata.tsv | cut -f 37 | xargs -n 1 curl -O -L
+# awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19" && $13=="SP1-human"' ../metadata.tsv | cut -f 37 | xargs -n 1 curl -O -L
 
-awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19"' metadata.tsv | cut -f 1,7,13,25 > hg19.bigwig.tsv
-awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19" && $13=="EP300-human"' ../metadata.tsv | cut -f 37 | xargs -n 1 curl -O -L
-awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19" && $13=="YY1-human"' ../metadata.tsv | cut -f 37 | xargs -n 1 curl -O -L
-awk -F"\t" '$2=="bigWig" && $3=="fold change over control" && $38=="hg19" && $13=="SP1-human"' ../metadata.tsv | cut -f 37 | xargs -n 1 curl -O -L
+################################################ HiChIP hichipper loops
+# awk '{print $1":"$2"-"$3"\t"$4":"$5"-"$6"\t"$8}' SRR5831489.filt.intra.loop_counts.bedpe > SRR5831489.filt.intra.loop_counts.bedpe.washU.txt
+# SRR5831489.intra.loop_counts.bedpe
+for pre in HKG v2.hkg.2 Brain Liver Spleen EBV Testis
+do
+	# bedtools pairtobed -a <(awk -vOFS="\t" '{print $1,$2,$3,$4,$5,$6,$7,$8}' SRR5831489.intra.loop_counts.bedpe) -b <(awk -vOFS="\t" '{a=$2-5000;b=$2+5000;if($6=="-"){a=$3-5000;b=$3+5000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' $pre.gene.bed) > $pre.hichipper.GM12878_cell_line.SRR5831489.pairtobed.txt
+	bedtools pairtobed -a <(awk -vOFS="\t" '$8 >= 5{print $1,$2,$3,$4,$5,$6,$7,$8}' SRR5831489.filt.intra.loop_counts.bedpe) -b <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' $pre.gene.bed) > $pre.hichipper.GM12878_cell_line.SRR5831489.pairtobed.txt
+done
+for pre in HKG v2.hkg.2 Brain Liver Spleen EBV Testis
+do
+	loopped=`cut -f 12 $pre.hichipper.GM12878_cell_line.SRR5831489.pairtobed.txt | sort | uniq | wc -l`
+	all=`cat $pre.gene.bed | wc -l`
+	perc=`echo $loopped/$all | bc -l`
+	echo $pre $all $loopped $perc
+done
+# HKG 472 430 .91101694915254237288
+# v2.hkg.2 2933 2625 .89498806682577565632
+# Brain 335 57 .17014925373134328358
+# Liver 216 50 .23148148148148148148
+# Spleen 246 85 .34552845528455284552
+# EBV 228 192 .84210526315789473684
+# Testis 1314 399 .30365296803652968036
+
+# awk '{print $1":"$2"-"$3"\t"$4":"$5"-"$6"\t"$8}' SRR5831489.filt.intra.loop_counts.bedpe > SRR5831489.filt.intra.loop_counts.bedpe.washU.txt
+for f in SRR5831489.filt.intra.loop_counts.bedpe
+do
+	awk -vOFS="\t" '$8 >= 5{print $1,$2,$3"\n"$4,$5,$6}' $f | bedtools sort -i - | uniq | awk -vOFS="\t" '{print $1,$2,$3,$1":"$2"-"$3}' | bedtools intersect -wo -a - -b <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' gencode.v19.gene.bed) > $f.gencode.txt
+	perl $myperl <(cut -f 4,8 $f.gencode.txt) <(awk '$3 >= 5' $f.washU.txt) 0 0 | cut -f 1-3,5 | perl $myperl <(cut -f 4,8 $f.gencode.txt) /dev/stdin 0 1 | cut -f 1-4,6 > $f.anntotation.txt
+	perl $myperl v2.hkg.tsg.anntotation.txt $f.anntotation.txt 0 3 | cut -f 1-5,7-8 | perl $myperl v2.hkg.tsg.anntotation.txt /dev/stdin 0 4 | cut -f 1-7,9-10 > $f.anntotation.hkg.tsg
+	awk -F"\t" -vOFS="\t" '$4!="/" && $5!="/" {print $4,$5,$6,$7,$8,$9}' $f.anntotation.hkg.tsg > $f.pp.txt
+	awk -F"\t" -vOFS="\t" '{if($4!="/" && $5=="/") {print $4,$2,$6,$7,$8,$9}}' $f.anntotation.hkg.tsg > $f.pe.txt
+	awk -F"\t" -vOFS="\t" '{if($4=="/" && $5!="/") {print $5,$1,$8,$9,$6,$7}}' $f.anntotation.hkg.tsg >> $f.pe.txt
+	# cut -f 1,3 $f.pe.txt | awk -F"\t" '$2!="/"' | sort | uniq | cut -f 2 | sort | uniq -c | sort -k 1,1nr
+	echo "Class Total promoter-promoter.count promoter-enhancer.count promoter-promoter% promoter-enhancer%"
+	for pre in HKG.1 HKG.2 Brain Liver Spleen EBV Testis
+	do
+		pcount=`awk -vvar=$pre -F"\t" '{if($3==var){print $1}if($4==var){print $2}}' $f.pp.txt | sort | uniq | wc -l`
+		ecount=`cut -f 1,3 $f.pe.txt | awk -F"\t" '$2!="/"' | sort | uniq | grep $pre | wc -l`
+		all=`grep $pre v2.hkg.tsg.anntotation.txt | wc -l`
+		pperc=`echo $pcount/$all | bc -l`
+		eperc=`echo $ecount/$all | bc -l`
+		echo $pre $all $pcount $ecount $pperc $eperc
+	done
+	pcount=`awk -F"\t" '{print $1"\n"$2}' $f.pp.txt | sort | uniq | wc -l`
+	ecount=`cut -f 1 $f.pe.txt | sort | uniq | wc -l`
+	all=`cat gencode.v19.gene.bed | wc -l`
+	pperc=`echo $pcount/$all | bc -l`
+	eperc=`echo $ecount/$all | bc -l`
+	echo gencode $all $pcount $ecount $pperc $eperc
+
+	pre=protein_coding
+	pcount=`awk -vvar=$pre -F"\t" '{if($4==var){print $1}if($6==var){print $2}}' $f.pp.txt | sort | uniq | wc -l`
+	ecount=`cut -f 1,4 $f.pe.txt | awk -F"\t" '$2!="/"' | sort | uniq | grep $pre | wc -l`
+	all=`grep protein_coding gencode.v19.gene.anntotation.txt | wc -l`
+	pperc=`echo $pcount/$all | bc -l`
+	eperc=`echo $ecount/$all | bc -l`
+	echo $pre $all $pcount $ecount $pperc $eperc
+
+	pcount=`awk -F"\t" '{print $1"\n"$2}' $f.pp.txt | sort | uniq | perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"' | wc -l`
+	ecount=`cut -f 1 $f.pe.txt | sort | uniq | perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"' | wc -l`
+	all=`awk '$22>2{print $1}' v2.log2tpm.median.tsv | wc -l`
+	pperc=`echo $pcount/$all | bc -l`
+	eperc=`echo $ecount/$all | bc -l`
+	echo EBV.expressed $all $pcount $ecount $pperc $eperc
+done
+
+########## compare loops between expressed and non-expressed genes
+for f in SRR5831489.filt.intra.loop_counts.bedpe
+do
+	mkdir loops.$f
+	awk -F"\t" '{print $1"\n"$2}' $f.pp.txt | sort | uniq | perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"{print $1}' | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- > loops.$f/EBV.expressed.pp.gene.bed
+	awk -F"\t" '{print $1"\n"$2}' $f.pp.txt | sort | uniq | perl $myperl <(awk '$22<1{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"{print $1}' | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- > loops.$f/EBV.nonexpressed.pp.gene.bed
+	cut -f 1 $f.pe.txt | sort | uniq | perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"{print $1}' | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- > loops.$f/EBV.expressed.pe.gene.bed
+	cut -f 1 $f.pe.txt | sort | uniq | perl $myperl <(awk '$22<1{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"{print $1}' | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- > loops.$f/EBV.nonexpressed.pe.gene.bed
+	cd loops.$f
+	for pre in EBV.expressed.pp EBV.nonexpressed.pp EBV.expressed.pe EBV.nonexpressed.pe
+	do
+		findMotifsGenome.pl <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' $pre.gene.bed) hg19 $pre.homer.motifs -size given -mknown /home1/04935/shaojf/myTools/HOMER/data/knownTFs/vertebrates/known.motifs 1> $pre.findMotifsGenome.txt 2>&1 &
+		annotatePeaks.pl <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' $pre.gene.bed) hg19 -m /home1/04935/shaojf/myTools/HOMER/data/knownTFs/vertebrates/known.motifs -size given 1> $pre.promoter.vert.motifs.txt 2> $pre.promoter.vert.motifs.log &
+	done
+	cd ../
+done
+########## compare loops between hkg, tsg, and other expressed genes
+perl $myperl <(cut -f 1 v2.hkg.tsg.txt) gencode.v19.gene.bed 0 3 | grep "/" | cut -f 1-6 > other.gene.bed
+perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) other.gene.bed 0 3 | grep -v "/" | cut -f 1-6 > EBV.expressed.other.gene.bed
+for f in SRR5831489.filt.intra.loop_counts.bedpe
+do
+	cd loops.$f/
+	perl $myperl EBV.expressed.pp.gene.bed <(cut -f 4 HKG.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.HKG1.pp.gene.bed
+	perl $myperl EBV.expressed.pe.gene.bed <(cut -f 4 HKG.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.HKG1.pe.gene.bed
+	perl $myperl EBV.expressed.pp.gene.bed <(cut -f 4 v2.hkg.2.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.HKG2.pp.gene.bed
+	perl $myperl EBV.expressed.pe.gene.bed <(cut -f 4 v2.hkg.2.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.HKG2.pe.gene.bed
+	perl $myperl EBV.expressed.pp.gene.bed <(cut -f 4 other.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.other.pp.gene.bed
+	perl $myperl EBV.expressed.pe.gene.bed <(cut -f 4 other.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.other.pe.gene.bed
+	perl $myperl EBV.expressed.pp.gene.bed <(cut -f 4 EBV.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.tsg.pp.gene.bed
+	perl $myperl EBV.expressed.pe.gene.bed <(cut -f 4 EBV.gene.bed) 3 0 | grep -v "/" | cut -f 2- > EBV.expressed.tsg.pe.gene.bed
+
+	perl $myperl <(cat EBV.expressed.pp.gene.bed EBV.expressed.pe.gene.bed | sort | uniq) <(cut -f 4 HKG.gene.bed) 3 0 | grep "/" | cut -f 1 | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- | grep -wv chrM > EBV.expressed.HKG1.noloop.gene.bed
+	perl $myperl <(cat EBV.expressed.pp.gene.bed EBV.expressed.pe.gene.bed | sort | uniq) <(cut -f 4 v2.hkg.2.gene.bed) 3 0 | grep "/" | cut -f 1 | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- | grep -wv chrM > EBV.expressed.HKG2.noloop.gene.bed
+	perl $myperl <(cat EBV.expressed.pp.gene.bed EBV.expressed.pe.gene.bed | sort | uniq) <(cut -f 4 EBV.expressed.other.gene.bed) 3 0 | grep "/" | cut -f 1 | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- | grep -wv chrM > EBV.expressed.other.noloop.gene.bed
+	perl $myperl <(cat EBV.expressed.pp.gene.bed EBV.expressed.pe.gene.bed | sort | uniq) <(cut -f 4 EBV.gene.bed) 3 0 | grep "/" | cut -f 1 | perl $myperl gencode.v19.gene.bed /dev/stdin 3 0 | cut -f 2- | grep -wv chrM > EBV.expressed.tsg.noloop.gene.bed
+
+	for g in EBV.expressed.*.*.gene.bed
+	do
+		pre=`echo $g | sed 's/.gene.bed//'`
+		findMotifsGenome.pl <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' $pre.gene.bed) hg19 $pre.homer.motifs -size given -mknown /home1/04935/shaojf/myTools/HOMER/data/knownTFs/vertebrates/known.motifs 1> $pre.findMotifsGenome.txt 2>&1 &
+		annotatePeaks.pl <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' $pre.gene.bed) hg19 -m /home1/04935/shaojf/myTools/HOMER/data/knownTFs/vertebrates/known.motifs -size given 1> $pre.promoter.vert.motifs.txt 2> $pre.promoter.vert.motifs.log &
+	done
+	cd ../
+done
+
+for pre in `ls *promoter.vert.motifs.txt | sed 's/.promoter.vert.motifs.txt//'`
+do
+	ets=`cut -f 1,103 $pre.promoter.vert.motifs.txt | tail -n +2 | awk -F"\t" '$2~/,/' | wc -l`
+	all=`tail -n +2 $pre.promoter.vert.motifs.txt | wc -l`
+	perc=`echo $ets/$all | bc -l`
+	echo $pre $ets $all $perc
+done
+for pre in `ls *promoter.vert.motifs.txt | sed 's/.promoter.vert.motifs.txt//'`
+do
+	yy1=`cut -f 1,354 $pre.promoter.vert.motifs.txt | tail -n +2 | awk -F"\t" '$2~/,/' | wc -l`
+	all=`tail -n +2 $pre.promoter.vert.motifs.txt | wc -l`
+	perc=`echo $yy1/$all | bc -l`
+	echo $pre $yy1 $all $perc
+done
+
+echo "GeneType Motif MotifCount GeneCount GeneWithMotif" | tr " " "\t" > homer.vert.motif.percentage.txt
+for pre in `ls *promoter.vert.motifs.txt | sed 's/.promoter.vert.motifs.txt//'`
+do
+	all=`tail -n +2 $pre.promoter.vert.motifs.txt | wc -l`
+	for i in `seq 22 384`
+	do
+		motif=`head_line $pre.promoter.vert.motifs.txt | awk -vvar=$i '$1==var{print $2}' | awk -F"/" '{print $1"/"$2}'`
+		mcount=`cut -f 1,$i $pre.promoter.vert.motifs.txt | tail -n +2 | awk -F"\t" '$2~/,/' | wc -l`
+		perc=`echo $mcount/$all | bc -l`
+		echo $pre $motif $mcount $all $perc | tr " " "\t" >> homer.vert.motif.percentage.txt
+	done
+done
+perl ~/myTools/BioinformaticsDaily/textProcess/make_matrixwith3col_from_single_file.pl <(awk -F"\t" -vOFS="\t" '{print $2,$1,$5}' homer.vert.motif.percentage.txt | tail -n +2) > homer.vert.motif.percentage.mat
+# ####################################################
+# args <- c("homer.vert.motif.percentage.mat")
+# library(pheatmap)
+# library(data.table)
+# input <- fread(args[1], sep = "\t", header = T, na.strings = "/")
+# scores <- data.matrix(input[,-1])
+# colors <- colorRampPalette(c("white", "red"))(10)
+# rownames(scores) <- as.matrix(input[,1])[,1]
+
+# pdf(file = paste(args[1], "pheatmap.pdf", sep = "."), width = 10, height = 16)
+# myplot <- pheatmap(scores, scale = "none", fontsize_row = 4, 
+# 	show_rownames = T, show_colnames = T, color = colors,
+# 	cluster_cols = T, cluster_rows = T)
+# dev.off()
+# diff.scores <- scores[(scores[,3] > 0.2 | scores[,7] > 0.2) & (scores[,3]/(scores[,7]+1e-10) > 1.5 | scores[,3]/(scores[,7]+1e-10) < 0.67), ]
+# pdf(file = paste(args[1], "diff.pheatmap.pdf", sep = "."), width = 10, height = 8)
+# myplot <- pheatmap(diff.scores, scale = "none", fontsize_row = 8, 
+# 	show_rownames = T, show_colnames = T, color = colors,
+# 	cluster_cols = T, cluster_rows = T)
+# dev.off()
+# score.min <- apply(scores, 1, min)
+# score.max <- apply(scores, 1, max)
+# diff.scores.1 <- scores[score.max / (score.min + 1e-10) > 2 & score.max > 0.2, ]
+# pdf(file = paste(args[1], "diff.1.pheatmap.pdf", sep = "."), width = 10, height = 8)
+# myplot <- pheatmap(diff.scores.1, scale = "none", fontsize_row = 8, 
+# 	show_rownames = T, show_colnames = T, color = colors,
+# 	cluster_cols = T, cluster_rows = T)
+# dev.off()
+# ####################################################
+
---
+++ housekeeping/hkg.loop.features.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# rclone sync -L /home1/04935/shaojf/scratch/HiChIP.test/hkg2loops/ mygoogle:hkg_tsg/HiChIP.test/hkg2loops/
+# args <- c("v2", "GTEx_sample.tissue.txt", "0.15", "1.5", "2", "0.25")
+# outputpre <- args[1]
+# tau.threshold <- as.numeric(args[3])
+# sd.threshold <- as.numeric(args[4])
+# median.threshold <- as.numeric(args[5])
+# tau.threshold2 <- as.numeric(args[6])
+myperl=/home1/04935/shaojf/myTools/BioinformaticsDaily/textProcess/add_any_2files_together.pl
+
+for input in SRR5831489:GM12878_cell_line SRR5831492:K562_cell_line SRR5831496:Naive_T_primary_cell SRR5831505:Treg_primary_cell
+do
+	echo $input
+	acc=`echo $input | awk -F":" '{print $1}'`
+	cell=`echo $input | awk -F":" '{print $2}'`
+	f=$acc.filt.intra.loop_counts.bedpe
+	awk '{print $1":"$2"-"$3"\t"$4":"$5"-"$6"\t"$8}' $acc.filt.intra.loop_counts.bedpe > $acc.filt.intra.loop_counts.bedpe.washU.txt
+	awk -vOFS="\t" '$8 >= 5{print $1,$2,$3"\n"$4,$5,$6}' $f | bedtools sort -i - | uniq | awk -vOFS="\t" '{print $1,$2,$3,$1":"$2"-"$3}' | bedtools intersect -wo -a - -b <(awk -vOFS="\t" '{a=$2-1000;b=$2+1000;if($6=="-"){a=$3-1000;b=$3+1000}if(a<0){a=0;}print $1,a,b,$4,"1000",$6}' gencode.v19.gene.bed) > $f.gencode.txt
+	perl $myperl <(cut -f 4,8 $f.gencode.txt) <(awk '$3 >= 5' $f.washU.txt) 0 0 | cut -f 1-3,5 | perl $myperl <(cut -f 4,8 $f.gencode.txt) /dev/stdin 0 1 | cut -f 1-4,6 > $f.anntotation.txt
+	perl $myperl v2.hkg.tsg.anntotation.txt $f.anntotation.txt 0 3 | cut -f 1-5,7-8 | perl $myperl v2.hkg.tsg.anntotation.txt /dev/stdin 0 4 | cut -f 1-7,9-10 > $f.anntotation.hkg.tsg
+	awk -F"\t" -vOFS="\t" '$4!="/" && $5!="/" {print $4,$5,$6,$7,$8,$9}' $f.anntotation.hkg.tsg > $f.pp.txt
+	awk -F"\t" -vOFS="\t" '{if($4!="/" && $5=="/") {print $4,$2,$6,$7,$8,$9}}' $f.anntotation.hkg.tsg > $f.pe.txt
+	awk -F"\t" -vOFS="\t" '{if($4=="/" && $5!="/") {print $5,$1,$8,$9,$6,$7}}' $f.anntotation.hkg.tsg >> $f.pe.txt
+
+	echo "Class Total promoter-promoter.count promoter-enhancer.count promoter-promoter% promoter-enhancer%"
+	for pre in HKG.1 HKG.2 Brain Liver Spleen EBV Testis
+	do
+		pcount=`awk -vvar=$pre -F"\t" '{if($3~var){print $1}if($4~var){print $2}}' $f.pp.txt | sort | uniq | wc -l`
+		ecount=`cut -f 1,3 $f.pe.txt | awk -F"\t" '$2!="/"' | sort | uniq | grep $pre | wc -l`
+		all=`grep $pre v2.hkg.tsg.anntotation.txt | wc -l`
+		pperc=`echo $pcount/$all | bc -l`
+		eperc=`echo $ecount/$all | bc -l`
+		echo $pre $all $pcount $ecount $pperc $eperc
+	done
+	pcount=`awk -F"\t" '{print $1"\n"$2}' $f.pp.txt | sort | uniq | wc -l`
+	ecount=`cut -f 1 $f.pe.txt | sort | uniq | wc -l`
+	all=`cat gencode.v19.gene.bed | wc -l`
+	pperc=`echo $pcount/$all | bc -l`
+	eperc=`echo $ecount/$all | bc -l`
+	echo gencode $all $pcount $ecount $pperc $eperc
+
+	pre=protein_coding
+	pcount=`awk -vvar=$pre -F"\t" '{if($4==var){print $1}if($6==var){print $2}}' $f.pp.txt | sort | uniq | wc -l`
+	ecount=`cut -f 1,4 $f.pe.txt | awk -F"\t" '$2!="/"' | sort | uniq | grep $pre | wc -l`
+	all=`grep protein_coding gencode.v19.gene.anntotation.txt | wc -l`
+	pperc=`echo $pcount/$all | bc -l`
+	eperc=`echo $ecount/$all | bc -l`
+	echo $pre $all $pcount $ecount $pperc $eperc
+
+	pcount=`awk -F"\t" '{print $1"\n"$2}' $f.pp.txt | sort | uniq | perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"' | wc -l`
+	ecount=`cut -f 1 $f.pe.txt | sort | uniq | perl $myperl <(awk '$22>2{print $1}' v2.log2tpm.median.tsv) /dev/stdin 0 0 | awk '$2!="/"' | wc -l`
+	all=`awk '$22>2{print $1}' v2.log2tpm.median.tsv | wc -l`
+	pperc=`echo $pcount/$all | bc -l`
+	eperc=`echo $ecount/$all | bc -l`
+	echo EBV.expressed $all $pcount $ecount $pperc $eperc
+done
