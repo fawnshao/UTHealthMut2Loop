c3eab21135f9ddbca0068a6a53ebfc63a44e8c52
Fawn Shao <dreambetter@gmail.com>
2018-07-25 16:04:03 -0500

    chip

--- housekeeping/analyze.looping.network.pl
+++ housekeeping/analyze.looping.network.pl
@@ -15,17 +15,17 @@ while(<IN>){
 	# }
 	$degrees{$t[0]}++;
 	$degrees{$t[1]}++;
-	if(not exists $subnetworks{$t[0]} && not exists $subnetworks{$t[1]}){
+	if(! exists $subnetworks{$t[0]} && ! exists $subnetworks{$t[1]}){
 		$i++;
 		$subnetworks{$t[0]} = $i;
 		$subnetworks{$t[1]} = $i;
 		$nodes{$i} = $t[0]."\t".$t[1];
 	}
-	elsif(not exists $subnetworks{$t[0]} && exists $subnetworks{$t[1]}){
+	elsif(! exists $subnetworks{$t[0]} && exists $subnetworks{$t[1]}){
 		$subnetworks{$t[0]} = $subnetworks{$t[1]};
 		$nodes{$subnetworks{$t[1]}} .= "\t".$t[0];
 	}
-	elsif(exists $subnetworks{$t[0]} && not exists $subnetworks{$t[1]}){
+	elsif(exists $subnetworks{$t[0]} && ! exists $subnetworks{$t[1]}){
 		$subnetworks{$t[1]} = $subnetworks{$t[0]};
 		$nodes{$subnetworks{$t[0]}} .= "\t".$t[1];
 	}
