012af9516047f9130fc072f9a2e7a26fdda5ec97
Fawn Shao <dreambetter@gmail.com>
2018-08-15 12:36:17 -0500

    chip

--- housekeeping/analyze.looping.network.pl
+++ housekeeping/analyze.looping.network.pl
@@ -4,6 +4,9 @@
 %degrees = ();
 %subnetworks = ();
 %nodes = ();
+####### add a relationship between interaction and subnetwork
+%interaction = ();
+#######
 $i = 0;
 open(IN, $ARGV[0]) or die "can not open $ARGV[0]\n";
 <IN>;
@@ -30,6 +33,18 @@ while(<IN>){
 		$subnetworks{$t[1]} = $subnetworks{$t[0]};
 		$nodes{$subnetworks{$t[0]}} .= "\t".$t[1];
 	}
+	##### for the above processing, we would miss a situation which is:
+	##### both of the nodes have already been assigned to different subnetworks
+	##### that we need to merge them
+	##### for v2 modification, we will try to take this into consideration!!!!
+	elsif(exists $subnetworks{$t[0]} && exists $subnetworks{$t[1]} && $subnetworks{$t[0]} != $subnetworks{$t[1]}){
+		$nodes{$subnetworks{$t[0]}} .= "\t".$nodes{$subnetworks{$t[1]}};
+		@x = split(/\t/,$nodes{$subnetworks{$t[1]}});
+		foreach $xx(@x){
+			$subnetworks{$xx} = $subnetworks{$t[0]};
+		}
+		delete $nodes{$subnetworks{$t[1]}};
+	}
 }
 close(IN);
 
@@ -51,4 +66,16 @@ foreach $s(keys %nodes){
 	$size = scalar(@t);
 	print OUT "subnet.$s\t$size\t$nodes{$s}\n";
 }
+close(OUT);
+
+open(OUT, ">$ARGV[0].interaction2subnetworks.txt");
+open(IN, $ARGV[0]) or die "can not open $ARGV[0]\n";
+<IN>;
+while(<IN>){
+	chomp;
+	@t = split(/\t/);
+	$intnodes = $t[0]."\t".$t[1];
+	$interaction{$intnodes} = "subnet".$subnetworks{$t[0]};
+}
+close(IN);
 close(OUT);
